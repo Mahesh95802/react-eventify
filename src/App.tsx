1f1097553548fa66af6904f037d6e06e862894af
Maheshwaran K <maheshwarankirupa@gmail.com>
2023-03-01 14:40:23 +0530

    feat: add single event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css';
-import HomePage from './pages/home';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/homePage';
 
-// import React from 'react';
+import './App.css';
+import NotFoundPage from './pages/notFoundPage';
+import EventPage from './pages/eventPage';
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path='/:id' element={<EventPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
